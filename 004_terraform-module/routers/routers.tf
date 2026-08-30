cc2a2c2fbe4bf6bd8760ff5bfb332d81f3ec06c2
martinezmr <mmartinez1513@gmail.com>
2026-08-30 18:40:23 -0500

    more terraform

--- 004_terraform-module/routers/routers.tf
+++ 004_terraform-module/routers/routers.tf
@@ -1,10 +1,44 @@
+resource "iosxe_interface_ethernet" "r1gi2" {
+  type              = "GigabitEthernet"
+  name              = "2"
+  shutdown          = false
+  ipv4_address      = "192.168.2.1"
+  ipv4_address_mask = "255.255.255.0"
+}
+
+resource "iosxe_interface_ethernet" "gi3" {
+  type     = "GigabitEthernet"
+  name     = "3"
+  shutdown = false
+}
+
+resource "iosxe_interface_ethernet" "gi4" {
+  type     = "GigabitEthernet"
+  name     = "4"
+  shutdown = false
+}
+
 resource "iosxe_ospf" "ospf1" {
-    process_id = 1
-    networks = [
+  process_id = 1
+  networks = [
+    {
+      ip       = "192.168.2.0"
+      wildcard = "0.0.0.255"
+      area     = "0"
+    }
+  ]
+}
+
+resource "iosxe_access_list_extended" "my_first_acl" {
+    name = "my_first_acl"
+    entries = [
         {
-            ip = "192.168.2.0"
-            wildcard = "0.0.0.255"
-            area = 0
+            sequence = 10
+            remarks = "permit all traffic"
+            ace_rule_action = "permit"
+            ace_rule_protocol = "ip"
+            source_any = true
+            destination_any = true
         },
     ]
 }
